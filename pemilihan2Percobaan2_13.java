d7482a722ef4caf31b03d01b85727ee678f41f2a
TUGAS <aklilhikam6@gmail.com>
2024-10-04 00:16:55 +0700

    Percobaan 2

---
+++ pemilihan2Percobaan2_13.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class pemilihan2Percobaan2_13 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+       String menu;
+       String member;
+       int jumlah_beli;
+       int total_bayar = 0;
+       int harga = 0;
+       double diskon = 0.10;
+
+        System.out.println("-------------------------");
+        System.out.println("===== MENU KAFE JTI =====");
+        System.out.println("-------------------------");
+        System.out.println("1. Kopi");
+        System.out.println("2. Teh");
+        System.out.println("3. Roti");
+        System.out.println("-------------------------");
+        System.out.println("masukan angka dari menu yang dipilih = ");
+        menu = input.nextLine();
+        System.out.println("Apakah punya member (y/n) ? = ");
+        member = input.nextLine();
+        System.out.println("Masukkan jumlah pembelian: ");
+        jumlah_beli = input.nextInt();
+        System.out.println("-------------------------");
+        
+        if (menu.equals("1")) {
+            harga = 12000;
+        } else if (menu.equals("2")) {
+            harga = 7000;
+        } else if (menu.equals("3")) {
+            harga = 20000;
+        }
+        
+
+        if (member.equalsIgnoreCase("y")) {
+            System.out.println("Diskon: 10%");
+            total_bayar = (int) ((harga - (harga * diskon)) * jumlah_beli);
+        } else {
+            total_bayar = harga * jumlah_beli;
+        }
+
+        System.out.println("Total yang harus dibayar: " + total_bayar);
+
+    }
+    
+}
